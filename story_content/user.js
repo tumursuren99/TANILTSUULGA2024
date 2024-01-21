2d1c9cc4df3f0ff805391094dc33bd4915910b96
tumursuren99 <tumursuren99@outlook.com>
2024-01-21 22:16:28 +0800

    tumursuren99

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,17 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5oXQf8oVPpE":
+      case "6LD8Ij1nL3C":
         Script1();
         break;
-      case "5w8jg1fEWm1":
+      case "62ONPBrxLJu":
         Script2();
         break;
-      case "63mWcDsIcJw":
+      case "6nH6hxe0rJJ":
         Script3();
+        break;
+      case "6oHzXeoN1RF":
+        Script4();
         break;
   }
 }
@@ -71,5 +74,30 @@ formData.append('Score', score);
 var request = new XMLHttpRequest();
 request.open('POST', 'https://script.google.com/macros/s/AKfycbwLGen-oTiQLOHqKuvx1qHDyJvGZZd_Oj-Tq2T9ugoVHA_TS6CbkQj7BQRCWiIul6Zzrw/exec');
 request.send(formData);
+}
+
+function Script4()
+{
+  const player = GetPlayer();
+const nameJS = player.GetVar("nameSL");//Storyline variables assigned to JS variables
+const emailJS = player.GetVar("emailSL");
+const scoreJS = player.GetVar("scoreSL");
+const timeJS = new Date().toLocaleString('en-US')
+const url = "https://prod-60.southeastasia.logic.azure.com:443/workflows/df69561db3264a829ff47b09ed9da276/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=M15Dr5nIv-p6NYetOKchPN9ZX_CfgawHgib-u-1elb0";
+const xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+const data = JSON.stringify ({
+  "name": nameJS,
+  "email": emailJS,
+  "score": scoreJS,
+  "time": timeJS
+})
+xhr.send(data);
 }
 
